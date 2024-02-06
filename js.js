80477b5b0a634ded406b259846d61cd8a0faf808
Juan Casado Luna <juans.casado@gmail.com>
2024-02-06 12:31:19 -0400

    primer commit

---
+++ js.js
@@ -0,0 +1,9 @@
+console.log("Hola!");
+
+const mantenimiento = document.querySelector("#mantenimiento");
+const lavado = document.querySelector("#lavado");
+
+mantenimiento.src = "imagenes/hero-inferior/mantenimiento.png";
+lavado.src = "imagenes/hero-inferior/lavado.png";
+// foto_python.src = "imagenes/fotos-tech-inow/Python.png";
+
